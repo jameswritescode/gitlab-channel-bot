553febcad4844ff8711c72776d4ed393e1b289e8
James Newton <hello@jamesnewton.com>
2015-02-27 15:41:28 -0500

    would help if I told it it's nick

--- gitlab.rb
+++ gitlab.rb
@@ -8,6 +8,8 @@ config = YAML.load_file('gitlab.yml')
 
 bot = Cinch::Bot.new do
   configure do |c|
+    c.nick     = config['username']
+    c.realname = '#gitlab helper bot'
     c.server   = 'irc.freenode.net'
     c.channels = ['#gitlab']
 
