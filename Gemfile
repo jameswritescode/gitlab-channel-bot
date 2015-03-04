e3514f3507edb7fc7ae91ad29c5fa018ef2195bf
James Newton <hello@jamesnewton.com>
2015-03-03 20:09:14 -0500

    add sinatra so we can display the facts somewhere outside of IRC

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'cinch'
+gem 'sinatra'
 gem 'sqlite3'
+gem 'terminal-table'
 gem 'pry'
--- gitlab.rb
+++ gitlab.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'cinch'
+require 'sinatra'
 require 'sqlite3'
+require 'terminal-table'
 require 'yaml'
 require 'pry'
 
@@ -10,7 +12,7 @@ $database = SQLite3::Database.new($config['db'])
 
 Dir['plugins/*.rb'].each { |file| require_relative file }
 
-Cinch::Bot.new do
+$bot = Cinch::Bot.new do
   configure do |config|
     config.nick     = $config['username']
     config.realname = '#gitlab helper bot'
@@ -22,4 +24,18 @@ Cinch::Bot.new do
     config.sasl.username = $config['username']
     config.sasl.password = $config['password']
   end
-end.start
+end
+
+# Thread.new { $bot.start }
+
+Thread.new do
+  set :port, $config['sinatra_port']
+
+  get '/' do
+    headers 'Content-Type' => 'text/plain'
+
+    facts = $database.execute('SELECT word, fact FROM factoids')
+
+    Terminal::Table.new(headings: ['Word', 'Fact'], rows: facts).to_s
+  end
+end
